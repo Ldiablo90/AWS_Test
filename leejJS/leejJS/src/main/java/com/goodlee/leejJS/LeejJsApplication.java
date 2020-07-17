0fd69c1107b52503e7154693ac208052470ba726
lee jong seong <ldiablo90@gmail.com>
2020-07-17 09:56:55 +0900

    init

---
+++ leejJS/leejJS/src/main/java/com/goodlee/leejJS/LeejJsApplication.java
@@ -0,0 +1,13 @@
+package com.goodlee.leejJS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeejJsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeejJsApplication.class, args);
+	}
+
+}
---
+++ leejJS/leejJS/src/test/java/com/goodlee/leejJS/LeejJsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goodlee.leejJS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeejJsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
